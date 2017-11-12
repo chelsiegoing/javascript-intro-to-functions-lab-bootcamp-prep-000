19a4c514d7e0899e52960c2f6bda069ba81dd0ef
lydia <lydiahamil@yahoo.com>
2017-11-12 20:23:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
   function whisper(string) {
   return string.toLowerCase()
 }
-function logShout() {
+function logShout(shout) {
   console.log(HELLO)
 }
 function sayHiToGrandma(string){
